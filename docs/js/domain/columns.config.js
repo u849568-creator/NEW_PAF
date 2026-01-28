61410de43064305019cddfc4bbf0663c8a18a600
u849568-creator <u849568@groupe-casino.fr>
2026-01-28 14:31:02 +0100

    Create columns.config.js

---
+++ docs/js/domain/columns.config.js
@@ -0,0 +1,18 @@
+export const COLUMN_GROUPS = {
+  IDENTITE: {
+    label: "Identité",
+    columns: ["EAN", "LIBELLE", "CODE_S"]
+  },
+  NOMENCLATURE_MONOPRIX: {
+    label: "Nomenclature Monoprix",
+    columns: ["RAY_MON", "UG_MON"]
+  },
+  NOMENCLATURE_FRANPRIX: {
+    label: "Nomenclature Franprix",
+    columns: ["UNIVERS_FPX", "RAY_FPX", "SS_GROUPE_FPX"]
+  },
+  NOMENCLATURE_CASINO: {
+    label: "Nomenclature Casino",
+    columns: ["UE_CASINO", "FAMILLE_CASINO"]
+  }
+};
