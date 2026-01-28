f5bafc456963b49e2c3463687062d1068122cba6
u849568-creator <u849568@groupe-casino.fr>
2026-01-28 13:14:36 +0100

    Create nomenclature.model.js

---
+++ docs/js/domain/nomenclature.model.js
@@ -0,0 +1,25 @@
+export function createEmptyNomenclature() {
+  return {
+    CIRCANA: {
+      MARCHE: "",
+      RAYON: "",
+      GROUPE_FAMILLE: "",
+      CATEGORIE: "",
+      TYPE: "",
+      SS_TYPE: ""
+    },
+    MONOPRIX: {
+      RAYON: "",
+      UG: ""
+    },
+    FRANPRIX: {
+      UNIVERS: "",
+      RAYON: "",
+      SS_GROUPE: ""
+    },
+    CASINO: {
+      UE: "",
+      FAMILLE: ""
+    }
+  };
+}
